861899849c4c0250172bcc96159ec868a18d299b
Egbert Voigt <Egbert.Voigt@gmx.de>
2016-03-06 16:41:41 +0100

    useless model element removed

--- src/GUIBuilder.d
+++ src/GUIBuilder.d
@@ -77,7 +77,7 @@ class GUIBuilder
 
         if (w !is null)
             {
-                w.setTitle("DABZilla Version 0.5.0");
+                w.setTitle("DABZilla Version 0.5.1");
                 w.addOnHide( delegate void(Widget aux){ exit(0); } );
 
                 scaleVolume = cast(Scale)g.getObject("scaleVolume");
